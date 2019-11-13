bf2af3759a3ced92867684673707fc6ef494077f
AlexCanova <alexcanova2002@gmail.com>
2019-11-13 09:56:28 +0100

    completamento programmi java

--- TernaPitagorica/src/ternapitagorica/TernaPitagorica.java
+++ TernaPitagorica/src/ternapitagorica/TernaPitagorica.java
@@ -1,24 +1,28 @@
-
 package ternapitagorica;
+
 import java.util.Scanner;
 
 public class TernaPitagorica {
 
     public static void main(String[] args) {
-        double a;
-        double b;
-        double c;
-        Scanner tastiera =  new Scanner (System.in);
+        int a;
+        int b;
+        int c;
+        Scanner tastiera = new Scanner(System.in);
         System.out.print("a = ");
-        a = tastiera.nextDouble();
-         System.out.print("b = ");
-        b = tastiera.nextDouble();
+        a = tastiera.nextInt();
+        System.out.print("b = ");
+        b = tastiera.nextInt();
         System.out.print("c = ");
-        c = tastiera.nextDouble();
+        c = tastiera.nextInt();
         
-        if (){
-            
+        if (a*a+b*b==c*c){
+            System.out.print("Questi numeri formanoo una terna pitagorica.");
         }
+            else {
+            System.out.print("Questi numeri non formano una terna pitagorica!");
+        }
+      2
     }
-    
+
 }
--- Triangolo/src/triangolo/Triangolo.java
+++ Triangolo/src/triangolo/Triangolo.java
@@ -1,18 +1,19 @@
-
 package triangolo;
+
 import java.util.Scanner;
 
 public class Triangolo {
-   
+
     public static void main(String[] args) {
         double i;
         double n;
-        int i = 0;
+        
         Scanner NumeriTriangolo = new Scanner(System.in);
         System.out.println("Inserisci numero:");
-        int n = NumeriTriangolo.nextInt();
-        
-        for (i < n; i = 5; i++)
-         System.out.println(i);
-         
+        n = NumeriTriangolo.nextInt();
+
+        for (i = 0; i <= n; i++) {
+            
+        }
+    }
 }
