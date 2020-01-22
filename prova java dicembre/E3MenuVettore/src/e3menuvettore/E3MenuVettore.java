555542a16397794c57a6a40f462efeef17eddcd1
AlexCanova <alexcanova2002@gmail.com>
2020-01-22 09:49:15 +0100

    es3_dacompletare

--- prova java dicembre/E3MenuVettore/src/e3menuvettore/E3MenuVettore.java
+++ prova java dicembre/E3MenuVettore/src/e3menuvettore/E3MenuVettore.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package e3menuvettore;
-
-/**
- *
- * @author ALEX.CANOVA
- */
+import java.util.Scanner;
 public class E3MenuVettore {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main(String[] args){
+     /*double[] v;
+        v = new double [10];
+        v [0] = 1.5;
+        v [1] = 3.0;
+        v [2] = 4.5;
+        v [3] = 3.0;
+        
+        double [] y = new double[10];
+        y [0] = 1.5;*/
+        
+        double [] z= {1.3, 3.0, 4.5, 3.0, 0.0, 0.0, 0.0, 0.0,};
+        Scanner tastiera = new Scanner (System.in);
+        String scelta;
+        do{
+            System.out.println("0-Uscita");
+            System.out.println("1-Visualizza vettore");
+            System.out.println("2-Leggi nuovo valore");
+            System.out.println("3-Visualizza media");
+            System.out.println("4-Visualizza i valori maggiori della media");
+            System.out.print("Scelta: ");
+            scelta = tastiera.nextLine();
+            
+            if (scelta.equals("1")) {
+                System.out.println("Hai scelto 1");
+            }
+        } while (!scelta.equals("0"));
     }
     
 }
